06db18c3a166b2721fa0eb896f9229d7b8694240
Luca Coviello <luca.coviello@gmail.com>
2016-12-08 19:09:11 +0100

    added params in topology

--- lucnik/src/main/java/master2016/Top3App.java
+++ lucnik/src/main/java/master2016/Top3App.java
@@ -7,6 +7,27 @@ import org.apache.storm.tuple.Fields;
 
 public class Top3App {
     public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            // TODO only for debug, then remove it
+
+            // TODO need to pass them to some bolt
+            String langlist = "en:house,it:casa,de:kartoffeln,es:ordenador";
+
+            // TODO need to start using ti
+            String kafkaBrokerUrl = "";
+            String topologyName = "hello-storm";
+
+            // TODO pass to the hashtag counter bolt
+            String outputFolder = "./";
+        } else {
+            String langlist = args[1];
+            String kafkaBrokerUrl = args[2];
+            String topologyName = args[3];
+            String outputFolder = args[4];
+        }
+
+        String[] langWithKeywords = langlist.split(",");
+
         Config config = new Config();
         config.setDebug(true);
 
@@ -24,7 +45,7 @@ public class Top3App {
         //         .shuffleGrouping("twitter-spout");
 
         LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("TwitterHashtagStorm", config,
+        cluster.submitTopology(topologyName, config,
                 builder.createTopology());
         Thread.sleep(60 * 1000);
         cluster.shutdown();
